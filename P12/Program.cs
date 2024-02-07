dd88dcc5d785c966655259edaf67d2d377e22f56
sabatex <serhiy.lakas@hotmail.com>
2024-02-07 21:25:26 +0200

    lesson 2

--- P12/Program.cs
+++ P12/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Data.Common;
+using System.Data;
+using Microsoft.Extensions.Configuration;
+
 Console.WriteLine("Hello, World!");
+
+var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").AddUserSecrets<Program>().Build();
+
+
+using (DbConnection connection = new Microsoft.Data.SqlClient.SqlConnection(config.GetConnectionString("DefaultConnection")))
+{
+    connection.Open();
+
+    var cmd = connection.CreateCommand();
+    cmd.CommandText = "select * from [dbo].[Invoices]";
+    var r = cmd.ExecuteReader();
+
+    while (r.Read())
+    {
+        var id = r.GetInt32("id");
+        var orderNumber = r.GetString("OrderNumber");
+    }
+
+
+    //var transaction = connection.BeginTransaction();
+    //transaction.Commit();
+    //transaction.Rollback();
+
+
+    connection.Close();
+
+}
+//new Microsoft.Data.Sqlite.SqliteConnection("FileName='Demo.db'"))
