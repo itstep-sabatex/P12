5021f725687d104d3663b2cbebb2ae2221c0f9a8
sabatex <serhiy.lakas@hotmail.com>
2024-03-15 18:56:58 +0200

    Свмостійна робота

--- MatrixThreadCalc/Program.cs
+++ MatrixThreadCalc/Program.cs
@@ -14,3 +14,26 @@ void MultiplreOneElement(object? param)
     }
     matrixParams.c[matrixParams.i, matrixParams.j] = result;
 }
+
+double[,] MultipleMatrix(int dim, double[,] a, double[,] b, Action<int> progress)
+{
+    var result = new double[dim, dim];
+    for (int i = 0; i < dim; i++)
+    {
+        progress?.Invoke(i);
+        for (int j = 0; j < dim; j++)
+        {
+            MultiplreOneElement(new MatrixParams(dim, i, j, a, b, result, null));
+        }
+    }
+    return result;
+}
+
+// Є однопоточний варіант розрахунку множення матриць.
+// Метод MultiplreOneElement адаптований для виконання в багатопоточному режимі
+//  - Написати метод заповнення матриці зандомними значеннями.
+//  - Написати метод розрахуеку матриці в потоках в кількості ядер
+//  - Написати метод розрахунку матриці в окремих потоках по одному на один рядок
+//  - Написати метод розрахунку матриці кожної клітинки в окремому потоці
+//  - Зробити порівняння швидкості виконання кожного варіанта для матриць 100,1000,5000
+//  - Зробити висновки з даних результатів
