cee2267319fb6faa6932e89fc1afb66f2a5a11b3
sabatex <serhiy.lakas@hotmail.com>
2024-04-17 20:03:34 +0300

    *

--- WebClientDemo/Program.cs
+++ WebClientDemo/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using HttpClientDemo;
 using System.Net;
+using System.Text;
 using System.Text.Json;
+using WebClientDemo;
 
 Console.WriteLine("Hello, World!");
 
@@ -23,5 +25,78 @@ try
 }
 catch(Exception ex)
 {
+
+}
+
+string clientId = "jhsdjsd";
+string destinationId = "3434";
+string token = "";
+
+
+void PostQuery(string objectType, string objectId)
+{
+    var url = new Uri(host, "api/v0/queries");
+    var webClient = new WebClient();
+    webClient.Encoding = Encoding.UTF8;
+    webClient.Headers.Add("Content-Type", "application/json; charset=utf-8");
+    webClient.Headers.Add("accept", "*/*");
+    webClient.Headers.Add("clientId", clientId.ToString());
+    webClient.Headers.Add("destinationId", destinationId.ToString());
+    webClient.Headers.Add("apiToken", token);
+    try
+    {
+        var obj = new { objectType = objectType, objectId = objectId };
+        string t = JsonSerializer.Serialize(obj);
+        webClient.UploadString(url, "POST", t);
+    }
+    catch (Exception ex)
+    {
+        throw new Exception($"POST object to service : {ex.ToString()}");
+    }
+}
+
+QueryObject[] GetQueries(int take = 10)
+{
+    var url = new Uri(host, $"api/v0/queries?take={take}");
+    var webClient = new WebClient();
+    webClient.Encoding = Encoding.UTF8;
+    webClient.Headers.Add("Content-Type", "application/json; charset=utf-8");
+    webClient.Headers.Add("accept", "*/*");
+    webClient.Headers.Add("clientId", clientId.ToString());
+    webClient.Headers.Add("destinationId", destinationId.ToString());
+    webClient.Headers.Add("apiToken", token);
+    try
+    {
+        string s = webClient.DownloadString(url);
+        //var result = JsonConvert.DeserializeObject<ObjectExchange>(s,new JsonSerializerSettings {ContractResolver =new DefaultContractResolver { NamingStrategy=new CamelCaseNamingStrategy()} });
+        return JsonSerializer.Deserialize<QueryObject[]>(s);
+
+
+    }
+    catch (Exception ex)
+    {
+        throw new Exception($"GET queries from service : {ex.ToString()}");
+    }
+}
+
+
+void DeleteQuery(long id)
+{
+    var url = new Uri(host, $"api/v0/queries/{id}");
+    //var request = HttpWebRequest.Create(url);
+    var request = (HttpWebRequest)WebRequest.Create(url);
+    request.Method = "DELETE";
+    request.Accept = "*/*";
+    request.Headers["clientId"] = clientId.ToString();
+    request.Headers["apiToken"] = token;
+
+    try
+    {
+        var responce = request.GetResponse();
+    }
+    catch (Exception ex)
+    {
+        throw new Exception($"Error delete query from service : {ex.ToString()}");
+    }
 
 }
---
+++ WebClientDemo/QueryObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebClientDemo
+{
+    /// <summary>
+    /// обєкти які потрібно отримати з нода
+    /// </summary>
+    public class QueryObject
+    {
+        public long Id { get; set; }
+        public Guid Sender { get; set; }
+        public Guid Destination { get; set; }
+        public string ObjectId { get; set; }
+        public string ObjectType { get; set; }
+
+    }
+}
