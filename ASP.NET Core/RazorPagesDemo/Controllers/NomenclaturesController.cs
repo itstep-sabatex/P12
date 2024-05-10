4bc9cb9021059a407d28d886b7914b4b552adeb8
sabatex <serhiy.lakas@hotmail.com>
2024-05-10 19:21:10 +0300

    ajax

---
+++ ASP.NET Core/RazorPagesDemo/Controllers/NomenclaturesController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Cafe.Models;
+using RazorPagesDemo.Data;
+
+namespace RazorPagesDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NomenclaturesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NomenclaturesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Nomenclatures
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Nomenclature>>> GetNomenclature()
+        {
+            return await _context.Nomenclature.ToListAsync();
+        }
+
+        // GET: api/Nomenclatures/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Nomenclature>> GetNomenclature(int id)
+        {
+            var nomenclature = await _context.Nomenclature.FindAsync(id);
+
+            if (nomenclature == null)
+            {
+                return NotFound();
+            }
+
+            return nomenclature;
+        }
+
+        // PUT: api/Nomenclatures/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutNomenclature(int id, Nomenclature nomenclature)
+        {
+            if (id != nomenclature.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(nomenclature).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!NomenclatureExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Nomenclatures
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Nomenclature>> PostNomenclature(Nomenclature nomenclature)
+        {
+            _context.Nomenclature.Add(nomenclature);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetNomenclature", new { id = nomenclature.Id }, nomenclature);
+        }
+
+        // DELETE: api/Nomenclatures/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteNomenclature(int id)
+        {
+            var nomenclature = await _context.Nomenclature.FindAsync(id);
+            if (nomenclature == null)
+            {
+                return NotFound();
+            }
+
+            _context.Nomenclature.Remove(nomenclature);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool NomenclatureExists(int id)
+        {
+            return _context.Nomenclature.Any(e => e.Id == id);
+        }
+    }
+}
--- ASP.NET Core/RazorPagesDemo/wwwroot/js/site.js
+++ ASP.NET Core/RazorPagesDemo/wwwroot/js/site.js
@@ -2,8 +2,35 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-function getCount() {
-    $.getJSON("tools/getcount", { format: "json" }).done(function (data) {
-        return data.count;
-    })
+
+
+function fillTable() {
+
+    let idnomenclatureTBody = "nomenclatureTBody";
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', '/api/nomenclatures');
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+        let status = xhr.status;
+        if (status == 200) {
+            let data = xhr.response;
+            let tb = document.getElementById(idnomenclatureTBody);
+            tb.innerHTML = null;
+            for (let i = 0; i < data.length; i++) {
+                let tr = document.createElement('tr');
+                let id = "id" + data[i].id;
+                tr.id = id;
+                //tr.onclick = () => rowClick(id);
+                tb.appendChild(tr);
+                let tdName = document.createElement("td");
+                tr.appendChild(tdName);
+                tdName.innerHTML = data[i].name;
+                let tdPrice = document.createElement("td");
+                tdPrice.innerHTML = data[i].price;
+                tr.appendChild(tdPrice);
+            }
+        }
+
+    }
+    xhr.send();
 }
